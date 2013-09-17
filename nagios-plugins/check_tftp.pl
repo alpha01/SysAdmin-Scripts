e966b294765772f9d356cfe8793df83a7b527f84
Alpha01 <root@rubyninja.org>
2013-09-17 02:38:16 -0700

    check_tftp.pl Nagios Plugin

---
+++ nagios-plugins/check_tftp.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl -w
+
+# Tony Baltazar. root[@]rubyninja.org
+
+use strict;
+use Getopt::Long;
+
+
+
+
+my %options;
+GetOptions(\%options, "host|H:s", "port|p:i",  "file|f:s", "help");
+
+
+if ($options{help}) {
+	usage();
+	exit 0;
+} elsif ($options{host} && $options{port} && $options{file}) { 
+	chdir('/tmp');
+
+	my $cmd = `/usr/bin/tftp -R $options{port}:$options{port} $options{host} $options{port} -c get $options{file}`;
+	if ($? != 0) {
+		print "CRITICAL: $cmd";
+		exit 2;
+	} else {
+		if (! -z "/tmp/$options{file}" ) {
+			print "TFTP is ok.\n$cmd";
+			system("rm -f /tmp/$options{file}");
+			exit 0;
+		} else {
+			print "WARNING: $cmd";
+			system("rm -f /tmp/$options{file}");
+			exit 1;
+		}
+	}
+
+} else {
+	usage();
+}
+
+
+
+sub usage {
+print <<EOF;
+
+$0: TFTP monitor check Nagios plugin.
+
+Syntax: $0 [--help|-H=<TFTP server> --port=<TFTP Port> --file=<Test file>]
+
+   --host | -H  : TFTP server.
+   --port | -p  : TFTP Port.
+   --file | -m  : Test file that will be downloaded.
+   --help | -h  : This help message.
+
+EOF
+}
